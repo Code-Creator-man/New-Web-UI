27fe757c49fa51599f19f9967f24725229f941c2
Code-Creator-man <justforcodings@hotmail.com>
2025-03-20 19:32:18 +0500

    deformation

--- backend.py
+++ backend.py
@@ -1,1 +1,40 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',  # Add Django REST Framework
+    'myapp',  # Add your app
+]
+from django.db import models
 
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    from rest_framework import serializers
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'  # Include all fields
+        from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
+
+# List all tasks or create a new task
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+# Retrieve, update, or delete a specific task
+class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
